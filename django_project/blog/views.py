fa6a879db5f16f34334adc80f5b4e9bae265fbc8
KebisKim <https://github.com/Kebiskim/2023_javastudy.git>
2024-08-08 21:33:11 +0900

    2024-08-08 tutorial

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author' : 'Sangwoo Kim',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'August 27, 2018'
-    },
-    {
-        'author' : 'Sangwoo Kim2',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'August 28, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author' : 'Sangwoo Kim',
+#         'title' : 'Blog Post 1',
+#         'content' : 'First post content',
+#         'date_posted' : 'August 27, 2018'
+#     },
+#     {
+#         'author' : 'Sangwoo Kim2',
+#         'title' : 'Blog Post 2',
+#         'content' : 'Second post content',
+#         'date_posted' : 'August 28, 2018'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
